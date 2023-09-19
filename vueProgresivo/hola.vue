c420006b9aa3be391d3f791f1849098cf1028603
Walter Gerardo Gonzalez Sanchez <wgonzalez@credicorpcapital.com>
2023-09-19 10:58:36 -0500

    curso total de vue

---
+++ vueProgresivo/hola.vue
@@ -0,0 +1,18 @@
+<template>
+    <p>lorem ipsum dolor</p>
+    <div> {{ text }}</div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            text: "¡Hola!"
+        }
+    }
+}
+</script>
+<style>
+p {
+    color: red;
+}
+</style>
